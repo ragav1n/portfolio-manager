3deeb4687648602bf06bdaac91b59628e0a613e5
Pramath KP <127417001+Pramath1234@users.noreply.github.com>
2025-01-21 18:38:13 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -38,7 +38,7 @@ function App() {
           
           {/* Protected form route */}
           <Route
-            path="/user-details"
+            path="/form"
             element={
               <AuthGuard>
                 <UserDetailsForm />
